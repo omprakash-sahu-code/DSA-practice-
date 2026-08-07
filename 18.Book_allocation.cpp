7d626895f91372a3e6ca2c1d779828a74a3af098
Omprakash Sahu <omprakashsahu2067@gmail.com>
2026-08-07 11:39:14 +0530

    Book Allocation Completed

--- 18.Book_allocation.cpp
+++ 18.Book_allocation.cpp
@@ -2,10 +2,53 @@
 #include <vector>
 using namespace std;
 
+bool isValid(vector<int> &arr, int n, int m, int maxAllowed){
+    int stu= 1, pages= 0;
+    for(int i=0; i<n; i++){
+        if(arr[i]>maxAllowed){
+            return false;
+        }
+        if(pages + arr[i] <= maxAllowed){
+            pages+=arr[i];
+        }else{
+            stu++;
+            pages=arr[i];
+        }
+    }
+
+    return stu>m ? false:true;
+}
+
+int bookAllocate(vector<int> &arr,int n, int m){
+    if(m>n){
+        return -1;
+    }
+
+    int sum=0;
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+    
+    int st=0, end=sum, ans=-1;
+    while(st<=end){
+        int mid=st + (end-st)/2;
+        if(isValid(arr,n,m,mid)){
+            ans=mid;
+            end=mid-1;
+        }else{
+            st=mid+1;
+        }
+    }
+    return ans;
+}
+
 
 int main(){
-    vector <int> arr={5,3,1,6,7,9,2,8};
-    cout << "INDEX: ";
+    vector <int> arr={2,1,3,4};
+    int n=arr.size();
+    int m=2;
+    cout<< bookAllocate(arr,n,m);
+    
     
     return 0;
 }
